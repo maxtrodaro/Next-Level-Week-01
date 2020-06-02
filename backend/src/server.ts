23c63237224f9b75ab8ad0675968b3ecb32e07a9
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-02 19:25:30 -0300

    Feature: Implementing editorconfig and filtered method with query param

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,49 @@
-import express from "express";
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Hello Eduardo");
+/**
+ * Rota: Endereço completo da requisição
+ * Recurso: Qual entidade estamos acessando do sistema (/users)
+ *
+ * GET: Buscar uma ou mais informações do back-end
+ * POST: Criar uma nova informação no back-end
+ * PUT: Editar uma informação já existente no back-end
+ * DELETE: Deletar uma informação no back-end
+ *
+ * Request Param: Parâmetros que vem da própria rota que identificam um recurso
+ * Query Param:
+ */
 
-  response.json(["Eduardo", "Rodrigo"]);
+const users = ['Eduardo', 'Diego'];
+
+app.get('/users', (request, response) => {
+	const search = String(request.query.search);
+
+	const filteredUsers = search
+		? users.filter((user) => user.includes(search))
+		: users;
+
+	console.log(filteredUsers);
+
+	response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+	const id = Number(request.params.id);
+
+	const user = users[id];
+
+	return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+	const user = {
+		name: 'Eduardo',
+		email: 'dudurossigodoy2010@gmail.com',
+	};
+
+	return response.json(user);
 });
 
 app.listen(3333);
