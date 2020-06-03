1947cbf90052c14a131a4395fa999ffa0b1ee692
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-03 19:48:36 -0300

    Feature: Props example with Typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
 
+/**
+ * JSX: Sintaxe de XML dentro do Javascript
+ */
+
+import Header from "./Header";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header title="Hello World" />
+      <h1>Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;
