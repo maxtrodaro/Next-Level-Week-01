5b5f06e1563d5e36b78d47dbb9efb5cbf183b039
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-03 23:58:39 -0300

    Feature: Implementing Front end Ecoleta With ReactJS and TypeScript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,14 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 /**
  * JSX: Sintaxe de XML dentro do Javascript
  */
 
-import Header from "./Header";
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <div>
-      <Header title="Hello World" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>
-        Aumentar
-      </button>
-    </div>
-  );
+	return <Routes />;
 }
 
 export default App;
