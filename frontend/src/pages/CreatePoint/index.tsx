fbfbb29fbd4cbe2d6258624d36ebe3ab5be9587d
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2020-06-05 20:36:50 -0300

    Feature: upload files feature

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -9,6 +9,7 @@ import './styles.css';
 import logo from '../../assets/logo.svg';
 import api from '../../services/api';
 import CreatePointSucess from '../CreatePointSucess';
+import Dropzone from '../../components/Dropzone';
 
 /**
  * Quando a gente cria um array ou objeto: eu preciso
@@ -52,6 +53,7 @@ const CreatePoint = () => {
 		0,
 		0,
 	]);
+	const [selectedFile, setSelectedFile] = useState<File>();
 
 	const [createSucess, setCreateSucess] = useState<boolean>(false);
 
@@ -139,16 +141,20 @@ const CreatePoint = () => {
 		const [latitude, longitude] = selectedPosition;
 		const items = selectedItems;
 
-		const data = {
-			name,
-			email,
-			whatsapp,
-			uf,
-			city,
-			latitude,
-			longitude,
-			items,
-		};
+		const data = new FormData();
+
+		data.append('name', name);
+		data.append('email', email);
+		data.append('whatsapp', whatsapp);
+		data.append('uf', uf);
+		data.append('city', city);
+		data.append('latitude', String(latitude));
+		data.append('longitude', String(longitude));
+		data.append('items', items.join(','));
+
+		if (selectedFile) {
+			data.append('image', selectedFile);
+		}
 
 		await api.post('points', data);
 
@@ -176,6 +182,7 @@ const CreatePoint = () => {
 				<h1>
 					Cadastro do <br /> ponto de coleta
 				</h1>
+				<Dropzone onFileUploaded={setSelectedFile} />
 				<fieldset>
 					<legend>
 						<h2>Dados</h2>
